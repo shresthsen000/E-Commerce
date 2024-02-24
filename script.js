eb3127c554840cffd51e25a955f59ce60dc44bac
Shrestha Sengupta <shresthsen000@gmail.com>
2024-02-24 14:29:42 +0530

    code

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     // Hide loader on page load
-    const loader = document.getElementById('loader');
-    loader.style.display = 'none';
-
+    const loader = document.getElementById("loader");
+    loader.style.display = "none";
+  
     // Auto-slide function
-    const slider = document.querySelector('.slider');
-    const slides = document.querySelectorAll('.slide');
-    let currentIndex = 0;
-
-    function autoSlide() {
-        setInterval(() => {
-            currentIndex = (currentIndex + 1) % slides.length;
-
-            // Move the slider to the next slide
-            slider.style.transform = `translateX(-${currentIndex * 300}%)`;
-        }, 2000);
+    const slider = document.querySelector(".slider");
+    const slides = document.querySelectorAll(".slide");
+    const imageLength = slides.length;
+    let totalScroll = 0;
+    const delay = 2000;
+  
+    for (let i = 0; i < 1; i++) {
+      slider.insertAdjacentHTML("beforeend", slides[i].outerHTML);
     }
-
-    autoSlide();
-
-    const burgerMenu = document.querySelector('.burger-menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    burgerMenu.addEventListener('click', function () {
-        navLinks.classList.toggle('show');
+  
+    let autoScroll = setInterval(scrolling, delay);
+  
+    function scrolling() {
+      totalScroll++;
+      if (totalScroll == imageLength + 1) {
+        clearInterval(autoScroll);
+        totalScroll = 1;
+        slider.style.transition = "0s";
+        slider.style.left = "0";
+        autoScroll = setInterval(scrolling, delay);
+      }
+      const widthEl = document.querySelector(
+        ".slider > :first-child"
+      ).offsetWidth;
+      slider.style.left = `-${totalScroll * widthEl}px`;
+      slider.style.transition = "2s";
+    }
+  
+    const burgerMenu = document.querySelector(".burger-menu");
+    const navLinks = document.querySelector(".nav-links");
+  
+    burgerMenu.addEventListener("click", function () {
+      navLinks.classList.toggle("show");
     });
-});
+  });
+  
